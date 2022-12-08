9bf24df5a40e21526066e462791ed1a8aa2a3198
angelmarte <robotking134@gmail.com>
2022-12-08 08:56:45 -0500

    commit tree a6a2a0f1999f29b9c3e0d2c387065ebbab049dea

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize word
+     @word = word
+  end
+  def match words
+   words.select do |elem|
+    word.chars.sort == elem.chars.sort
+   end
+  end
+end
